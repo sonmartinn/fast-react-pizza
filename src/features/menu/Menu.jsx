ad9bc1c70c7944c7734951c9ee3d634c6c256e3a
Le Trung Son <sonmartin2003@gmail.com>
2025-08-03 15:01:51 +0700

    feat: Implement pizza ordering application with cart functionality

---
+++ src/features/menu/Menu.jsx
@@ -0,0 +1,23 @@
+import { useLoaderData } from 'react-router-dom'
+
+import MenuItem from './MenuItem'
+import { getMenu } from '../../services/apiRestaurant'
+
+function Menu() {
+  const menu = useLoaderData()
+  return (
+    <ul className="divide-y divide-stone-200 px-2">
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  )
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export async function loader() {
+  const menu = await getMenu()
+  return menu
+}
+
+export default Menu
---
+++ src/ui/Button.jsx
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom'
+
+function Button({ children, disabled, to, type, onClick }) {
+  const base =
+    'cursor-pointer inline-block text-sm rounded-full bg-yellow-400 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed '
+
+  const styles = {
+    primary: base + ' px-4 py-3 md:px-6 md:py-4',
+    small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+    round: base + ' px-2.5 py-1 md:px-3.5 md:py-2 text-sm',
+    secondary:
+      'px-4 py-2.5 md:px-6 md:py-3.5 cursor-pointer inline-block text-sm rounded-full border-2 border-stone-300 font-semibold tracking-wide text-stone-400 uppercase transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:ring focus:ring-stone-200 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed'
+  }
+
+  if (to)
+    return (
+      <Link to={to} className={styles[type]}>
+        {children}
+      </Link>
+    )
+
+  if (onClick)
+    return (
+      <button disabled={disabled} className={styles[type]} onClick={onClick}>
+        {children}
+      </button>
+    )
+
+  return (
+    <button disabled={disabled} className={styles[type]}>
+      {children}
+    </button>
+  )
+}
+
+export default Button
